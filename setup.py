333eb8c6c3bb9f762a37a1984e302654fc679fce
pclazatin <patricia.lazatin@gmail.com>
2022-12-03 13:26:53 +0800

    creating a package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hours',
+    version='1.0',
+    packages=find_packages(),
+    url='https://github.com/pclazatin/hours.git',
+    description='use sqlgsheet to create and update blockytime events and post to gsheet',
+    author='@pclazatin',
+    long_description=open('README.md').read(),
+    install_requires=open("requirements.txt", "r").read().splitlines(),
+    include_package_data=True
+)
